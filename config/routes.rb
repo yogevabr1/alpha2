4060750258fca5b22fa8f3d95b0fe5de9ad248e2
Rails course <yogevabr@gmail.com>
2018-04-29 00:16:06 +0300

    new template action and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
 	root "pages#home"
 	get "about", to: "pages#about"
+
+	resources :articles # new article path for everything
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
