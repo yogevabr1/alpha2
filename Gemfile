bc27d149934c99e910574eacf54f999c9fe8cf28
Rails course <yogevabr@gmail.com>
2018-04-29 23:54:44 +0300

    fdgdfgdf

--- Gemfile
+++ Gemfile
@@ -9,12 +9,14 @@ gem 'rails', '~> 5.2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
+gem 'bootstrap-sass', '~> 3.3.5'
+
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
-
+gem 'jquery-rails'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -25,7 +27,6 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,48 @@
 class ArticlesController < ApplicationController
 
+def index
+	@articles = Article.all
+end
+
 def new
 	@article = Article.new
 
 end
 
+def edit
+	@article = Article.find(params[:id])
+end
+
+def update
+	@article = Article.find(params[:id])
+	if @article.update(article_params)
+		flash[:success] = "Article was succesfully updated"
+		redirect_to article_path(@article)
+	else
+		render 'edit'
+	end
+end
+
 def create
 	@article = Article.new(article_params)
-	@article.save
-	redirect_to articles_show(@article)
+	if @article.save
+		flash[:sucess] = "Article was succesfully created"
+		redirect_to article_path(@article)
+	else
+		render 'new'
+	end
+end
+
+
+def show
+	@article = Article.find(params[:id])
+end
+
+def destroy
+	@article = Article.find(params[:id])
+	@article.destroy
+	flash[:danger] = "Article was succesfully deleted"
+	redirect_to articles_path
 end
 
 
