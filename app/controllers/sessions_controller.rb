ec3e37b25d9410de4c76b783bd2c6eefa917062c
Rails course <yogevabr@gmail.com>
2018-05-02 08:16:59 +0300

    login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email_field].downcase)
+    if user && user.authenticate(params[:session][:password_field])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Wrong email or password"
+      render 'new'
+    end
+
+  end
+
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out" 
+    redirect_to root_path
+  end
+
+end
